940ab745cd73028403827f781ef7762f87437f0a
Ali Marzak <ali.marzak@live.fr>
2024-03-12 10:21:47 +0100

    Ajout de l'exo 5 terminé

--- exo5/js/main.js
+++ exo5/js/main.js
@@ -1,6 +1,23 @@
-input = document.getElementById("convert").ariaValueMax;
+function convertir(euro){
+    result = (euro * 6.55957).toFixed(2) // Arrondi à 2 décimal
+    return result
+}
 
 const box_result = document.createElement("div") // Creation d'une div
 box_result.classList.add("box") //Ajout d'une classe sur l'élément
-body.appendChild(box_result)
-box_result.innerText = input
+
+
+document.addEventListener('keyup', function(){
+    input = document.getElementById("convert").value;
+
+    console.log(input)
+    document.getElementById("container").appendChild(box_result)
+    if (isNaN(input)){
+        box_result.innerText = "Vous devez saisir une valeur numérique"
+    }
+    else{
+        box_result.innerText = convertir(input) + " francs"
+    }
+})
+
+
